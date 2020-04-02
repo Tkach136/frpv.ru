cadad45d6267515159f1fbbb0400b39f509d8d21
ezenkin <ezenkin@it-russ.com>
2020-04-02 11:13:53 +0300

    добавил бд в отслеживание

--- project/frpv/urls.py
+++ project/frpv/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 app_name = 'frpv'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('send/', views.send, name='send'),
     path('application/', views.application, name='application'),
     path('navigator/', views.navigator, name='navigator'),
     path('archive/', views.archive, name='archive'),
